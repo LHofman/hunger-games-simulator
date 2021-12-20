dc79691813625a425bd20bef70ec1658d7bbbd58
LHofman <hofmanlennert@gmail.com>
2021-12-20 14:41:05 +0100

    small improvements

--- src/index.py
+++ src/index.py
@@ -5,62 +5,62 @@ import vars
 from playGame import *
 
 def readFile(fileName, type = "text"):
-    file = open("settings/%s" % fileName, "r")
-    
-    if (type == 'json'):
-        return json.load(file)
+  file = open("settings/%s" % fileName, "r")
+  
+  if (type == 'json'):
+    return json.load(file)
 
-    lines = file.readlines()
-    return list(map(lambda line: line.rstrip(), lines))
+  lines = file.readlines()
+  return list(map(lambda line: line.rstrip(), lines))
 
 def readTributes():
-    file = open("settings/tributes.txt", "r")
-    lines = file.readlines()
+  file = open("settings/tributes.txt", "r")
+  lines = file.readlines()
 
-    if (vars.gameData["options"]["districts"] > 0):
-        playersPerDistrict = len(lines) / vars.gameData["options"]["districts"]
-    elif (vars.gameData["options"]["playersPerDistrict"] > 0):
-        playersPerDistrict = vars.gameData["options"]["playersPerDistrict"]
-    else: playersPerDistrict = 0
-    
-    tributes = {}
-    players = 0
-    for line in lines:
-        players += 1
-        name = line.rstrip()
-        tributes[name] = {
-            "index": players,
-            "name": name, 
-            "district": int(((players-1)/playersPerDistrict)+1) if (playersPerDistrict > 0) else 0,
-            "items": [],
-            "statuses": {},
-            "groupedWith": []
-        }
+  if (vars.gameData["options"]["districts"] > 0):
+    playersPerDistrict = len(lines) / vars.gameData["options"]["districts"]
+  elif (vars.gameData["options"]["playersPerDistrict"] > 0):
+    playersPerDistrict = vars.gameData["options"]["playersPerDistrict"]
+  else: playersPerDistrict = 0
+  
+  tributes = {}
+  players = 0
+  for line in lines:
+    players += 1
+    name = line.rstrip()
+    tributes[name] = {
+      "index": players,
+      "name": name, 
+      "district": int(((players-1)/playersPerDistrict)+1) if (playersPerDistrict > 0) else 0,
+      "items": [],
+      "statuses": {},
+      "groupedWith": []
+    }
 
-    if (vars.gameData["options"]["districtsAreTeammates"]):
-        for name, tribute in list(tributes.items()):
-            for name2, tribute2 in list(tributes.items()):
-                if (name2 != name and tribute2["district"] == tribute["district"]):
-                    tribute["groupedWith"].append(name2)
+  if (vars.gameData["options"]["districtsAreTeammates"]):
+    for name, tribute in list(tributes.items()):
+      for name2, tribute2 in list(tributes.items()):
+        if (name2 != name and tribute2["district"] == tribute["district"]):
+          tribute["groupedWith"].append(name2)
 
-    return tributes
+  return tributes
 
 
 def printWinner():
-    if (len(vars.tributes) == 1): print("The winner is %s" % list(vars.tributes.keys())[0])
-    elif (len(vars.tributes) > 1): print("The winners are %s" % ", ".join(list(vars.tributes.keys())))
-    else: print("There are no winners today")
+  if (len(vars.tributes) == 1): print("The winner is %s" % list(vars.tributes.keys())[0])
+  elif (len(vars.tributes) > 1): print("The winners are %s" % ", ".join(list(vars.tributes.keys())))
+  else: print("There are no winners today")
 
 def printRankings():
-    print("\n\n\n---\nFinal Rankings")
+  print("\n\n\n---\nFinal Rankings")
 
-    for playerDeaths in vars.deaths:
-        for (player, district) in playerDeaths:
-            print("%d. %s from district %d" % (vars.totalTributes, player, district))
-            vars.totalTributes -= 1
-    
-    for name, tribute in list(vars.tributes.items()):
-        print("1. %s from district %d" % (name, tribute["district"]))
+  for playerDeaths in vars.deaths:
+    for (player, district) in playerDeaths:
+      print("%d. %s from district %d" % (vars.totalTributes, player, district))
+      vars.totalTributes -= 1
+  
+  for name, tribute in list(vars.tributes.items()):
+    print("1. %s from district %d" % (name, tribute["district"]))
 
 print('----------------------------------------------------------------------------------------------------------------')
 vars.gameData = readFile("gameData.json", "json")
--- src/playGame.py
+++ src/playGame.py
@@ -7,127 +7,127 @@ from getEventTextAndPlayers import *
 from handleEventEffects import *
 
 def playGame():
-    playRound("The Bloodbath", "bloodbath", False)
-    day = 0
-    while (not isGameOver()):
-        day += 1
-        playRound("Day %d" % day, "day", True)
-        showFallenTributes()
-        playRound("Night %d" % day, "night", True)
-        if (day == 5): playRound("The Feast", "feast", False)
+  playRound("The Bloodbath", "bloodbath", False)
+  day = 0
+  while (not isGameOver()):
+    day += 1
+    playRound("Day %d" % day, "day", True)
+    showFallenTributes()
+    playRound("Night %d" % day, "night", True)
+    if (day == 5): playRound("The Feast", "feast", False)
 
 def playRound(text, time, playStandardEvents):
-    if (isGameOver()): return
+  if (isGameOver()): return
 
-    shuffleTributes()
-    readInput()
-    print(text)
-    playList(time, playStandardEvents)
-    print('\n---')
+  shuffleTributes()
+  readInput()
+  print(text)
+  playList(time, playStandardEvents)
+  print('\n---')
 
 def playList(time, playStandardEvents):
-    tributesLeft = vars.tributes.copy()
-    played = 0
-    total = amountLeft = len(tributesLeft)
-    while (amountLeft > 0 and len(vars.tributes) > 1):
-        checkEveryoneInTheSameGroup()
+  tributesLeft = vars.tributes.copy()
+  played = 0
+  total = amountLeft = len(tributesLeft)
+  while (amountLeft > 0 and len(vars.tributes) > 1):
+    checkEveryoneInTheSameGroup()
 
-        tribute = list(tributesLeft.values())[0]
-        del tributesLeft[tribute["name"]]
-        percentage = percentageOfPlaying(total, time)
-        rnd = random.random()
-        if (rnd < percentage):
-            playTribute(tribute, tributesLeft, time, playStandardEvents)
-            played += 1
-            
-        amountLeft = len(tributesLeft)
+    tribute = list(tributesLeft.values())[0]
+    del tributesLeft[tribute["name"]]
+    percentage = percentageOfPlaying(total, time)
+    rnd = random.random()
+    if (rnd < percentage):
+      playTribute(tribute, tributesLeft, time, playStandardEvents)
+      played += 1
+      
+    amountLeft = len(tributesLeft)
 
-    if (played == 0):
-        playList(vars.tributes)
+  if (played == 0):
+    playList(vars.tributes)
 
 def playTribute(tribute, playersRemaining, time, playStandardEvents):
-    event = getEvent(tribute, playersRemaining, time, playStandardEvents)
+  event = getEvent(tribute, playersRemaining, time, playStandardEvents)
 
-    (text, players) = getEventTextAndPlayers(event, tribute, playersRemaining)
-    
-    text = handleEventEffects(event, players, text)
-    
-    print(text)
+  (text, players) = getEventTextAndPlayers(event, tribute, playersRemaining)
+  
+  text = handleEventEffects(event, players, text)
+  
+  print(text)
 
 def readInput():
-    userInput = input("Press Enter to continue, or type status to see the current status of all tributes: ")
-    print("")
+  userInput = input("Press Enter to continue, or type status to see the current status of all tributes: ")
+  print("")
 
-    if (userInput == "stop"): exit()
+  if (userInput == "stop"): exit()
 
-    if (userInput == "status"):
-        printStatus()
-        readInput()
+  if (userInput == "status"):
+    printStatus()
+    readInput()
 
 def isGameOver():
-    if (len(vars.tributes) <= 1): return True
+  if (len(vars.tributes) <= 1): return True
 
-    if (vars.gameData["options"]["districtCanWinTogether"]):
-        isEveryoneInSameDistrict = True
-        for name, tribute in vars.tributes.items():
-            for name2, tribute2 in vars.tributes.items():
-                if (name2 != name and tribute2["district"] != tribute["district"]):
-                    isEveryoneInSameDistrict = False
-                    break
-            if (not isEveryoneInSameDistrict): break
-    
-        if (isEveryoneInSameDistrict): return True
+  if (vars.gameData["options"]["districtCanWinTogether"]):
+    isEveryoneInSameDistrict = True
+    for name, tribute in vars.tributes.items():
+      for name2, tribute2 in vars.tributes.items():
+        if (name2 != name and tribute2["district"] != tribute["district"]):
+          isEveryoneInSameDistrict = False
+          break
+      if (not isEveryoneInSameDistrict): break
+  
+    if (isEveryoneInSameDistrict): return True
 
-    return False
+  return False
 
 def shuffleTributes():
-    l = list(vars.tributes.items())
-    random.shuffle(l)
-    vars.tributes = dict(l)
+  l = list(vars.tributes.items())
+  random.shuffle(l)
+  vars.tributes = dict(l)
 
 def percentageOfPlaying(total, time):
-    if (time in ["bloodbath", "feast"]): return 1
-    
-    if (total > 25): return 15/total
-    if (total > 15): return 0.5
-    if (total > 5): return 0.75
-    return 1
+  if (time in ["bloodbath", "feast"]): return 1
+  
+  if (total > 25): return 15/total
+  if (total > 15): return 0.5
+  if (total > 5): return 0.75
+  return 1
 
 def checkEveryoneInTheSameGroup():
-    for name, tribute in vars.tributes.items():
-        for name2, tribute2 in vars.tributes.items():
-            if (name2 != name and name2 not in tribute["groupedWith"]): return
+  for name, tribute in vars.tributes.items():
+    for name2, tribute2 in vars.tributes.items():
+      if (name2 != name and name2 not in tribute["groupedWith"]): return
 
-    for name, _ in vars.tributes.items():
-        vars.tributes[name]["groupedWith"].clear()
+  for name, _ in vars.tributes.items():
+    vars.tributes[name]["groupedWith"].clear()
 
-    print("The remaining tributes realize they are the only ones left and split up")
+  print("The remaining tributes realize they are the only ones left and split up")
 
 def showFallenTributes():
-    if (len(vars.recentDeaths) > 0 and vars.gameData["options"]["showFallenTributes"]):
-        if (not isGameOver()): readInput()
-        print("%d cannon shots can be heard in the distance." % len(vars.recentDeaths))
-        for playerName, district in vars.recentDeaths:
-            print ("%s from district %d" % (playerName, district))
-        print("---")
-    
-    vars.deaths.append(vars.recentDeaths.copy())
-    vars.recentDeaths.clear()
+  if (len(vars.recentDeaths) > 0 and vars.gameData["options"]["showFallenTributes"]):
+    if (not isGameOver()): readInput()
+    print("%d cannon shots can be heard in the distance." % len(vars.recentDeaths))
+    for playerName, district in vars.recentDeaths:
+      print ("%s from district %d" % (playerName, district))
+    print("---")
+  
+  vars.deaths.append(vars.recentDeaths.copy())
+  vars.recentDeaths.clear()
 
 def printStatus():
-    for (name, tribute) in vars.tributes.items():
-        print("%s from district %d is still alive%s%s%s" % (
-            name,
-            tribute["district"],
-            "" if (len(tribute["statuses"]) == 0) else (
-                ", is: " + ", ".join(list(tribute["statuses"].keys()))
-            ),
-            "" if (len(tribute["items"]) == 0) else (
-                ", has: " + ", ".join(tribute["items"])
-            ),
-            "" if (len(tribute["groupedWith"]) == 0) else (
-                ", is in a group with: " + ", ".join(tribute["groupedWith"])
-            ),
-        ))
+  for (name, tribute) in vars.tributes.items():
+    print("%s from district %d is still alive%s%s%s" % (
+      name,
+      tribute["district"],
+      "" if (len(tribute["statuses"]) == 0) else (
+        ", is: " + ", ".join(list(tribute["statuses"].keys()))
+      ),
+      "" if (len(tribute["items"]) == 0) else (
+        ", has: " + ", ".join(tribute["items"])
+      ),
+      "" if (len(tribute["groupedWith"]) == 0) else (
+        ", is in a group with: " + ", ".join(tribute["groupedWith"])
+      ),
+    ))
 
-    print("\n---")
+  print("\n---")
