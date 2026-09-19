466c494387f75da94d306f01a7c946b7a220340b
Lennert Hofman <hofmanlennert@gmail.com>
2026-09-19 14:14:49 +0200

    rename function for clarity

--- src/Application/canPlayEvent.py
+++ src/Application/canPlayEvent.py
@@ -2,11 +2,11 @@ from Domain.eventRulesList import eventRulesList
 from Domain.types import Event, GameRoundStateWithoutEvent
 
 def canPlayEvent(gameState: GameRoundStateWithoutEvent):
-  def canPlayEvent(event: Event) -> bool:
+  def canPlayEventInner(event: Event) -> bool:
     for eventRule in eventRulesList:
       if not eventRule.canPlayEvent(event, gameState):
         return False
       
     return True
 
-  return canPlayEvent
+  return canPlayEventInner
